febfa64caab908c71dc47c2d52de8a4236134ac3
selvasathis <127232661+selvasathis@users.noreply.github.com>
2023-10-13 20:15:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static(__dirname + '/public'))
-
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
